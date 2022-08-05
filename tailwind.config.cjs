287815c3c265ef2bfe737b7b160b32cadfef666c
samithw <mailtosamith@gmail.com>
2022-08-05 14:27:29 +0530

    custom screen sizes added. changed md to 840px

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,22 @@ module.exports = {
           nsgreen : '#00FFC3'
       }
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '840px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
