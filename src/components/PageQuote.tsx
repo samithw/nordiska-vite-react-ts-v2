d30d5a7c0b0b563eee3369d75e3e3e66519850a6
samithw <mailtosamith@gmail.com>
2022-08-05 17:24:18 +0530

    mobile view

--- src/components/PageQuote.tsx
+++ src/components/PageQuote.tsx
@@ -6,10 +6,10 @@ const PageQuote = (props: Props) => {
   return (
     <div className="w-full" style={{backgroundImage: 'linear-gradient(45deg, #213039 50%, #162026 50%)'}}>
         <div className="md:container min-h-[60vh] mx-auto p-10 flex flex-col items-center justify-center">
-          <div className=" text-white text-5xl relative font-zenLoop text-center w-2/3 p-5">
-            <span className=" text-red-500 text-6xl font-zen absolute top-2 left-0 inline-block">"</span>
+          <div className=" text-white text-5xl relative font-zenLoop text-center lg:w-2/3 p-5">
+            {/* <span className=" text-red-500 text-6xl font-zen absolute top-2 left-0 inline-block">"</span> */}
             {props.children}
-            <span className=" text-rose-500 text-6xl font-zen absolute bottom-2 inline-block">"</span>
+            {/* <span className=" text-rose-500 text-6xl font-zen absolute bottom-2 inline-block">"</span> */}
           </div>
         </div>
       </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,13 +15,13 @@ const About = (props: Props) => {
         </PageQuote>
 
         <div className="w-full min-h-[80vh]" style={{backgroundImage: 'linear-gradient(45deg, #001140 30%, #010d31 70%)'}}>
-        <div className="md:container flex space-x-20 mx-auto p-10">
-          <div className="w-1/2 text-white font-zenMaru text-2xl">
+        <div className="md:container flex flex-col gap-20 md:flex-row mx-auto p-10">
+          <div className="text-white md:basis-1/2 font-zenMaru text-2xl">
             <h3 className="font-zen text-nsgreen text-4xl">history</h3>
             <p className="my-10">Nordiska Solutions is Finnish-Malaysian joint company which has developed a cluster business concept with some leading environmental companies in Europe and Japan who are active in water, wastewater and solids waste management technologies and solutions. </p>
             <p>This cluster business platform provides sophisticate and cost-effective solutions that are in the best interest of our clients, their customers and the environment. The founder of the company has over 22 years experiences in cleantech field and vast knowledge in international business.</p>
           </div>
-          <div className="w-1/2 text-white font-zenMaru text-2xl">
+          <div className="text-white md:basis-1/2 font-zenMaru text-2xl">
             <h3 className="font-zen text-nsgreen text-4xl">expertise</h3>
             <p className="my-10">The domain expertise &amp; experience gathered over past two decades gives us edge over competition so as to provide high-end technologies and solutions to various domestic and industrial applications.</p>
             <p className="my-10">We constantly continue developing our technology together with our customers and partners in order to guarantee the very highest level of customer satisfaction and sustainability.</p>
