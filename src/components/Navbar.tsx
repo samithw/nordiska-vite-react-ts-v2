ce324e976e332fc94cbc623c8aeacbf1731358a9
samithw <mailtosamith@gmail.com>
2022-08-09 13:48:38 +0530

    Mobile navbar close on click outside

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,38 @@
-import React, { MouseEventHandler, useRef, useState } from 'react'
+import React, { MouseEventHandler, useEffect, useRef, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 type Props = {}
 
+
+
+
 const Navbar = (props: Props) => {
 
   const [menuStatus, setMenuStatus] = useState(false)
-  //const mainNav = useRef(null)
 
-  let isHidden = menuStatus ? "" : "hidden"
+  const mainNavRef = useRef<HTMLUListElement>(null)
+  const isHidden = menuStatus ? "" : "hidden"
 
   const handleCLick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setMenuStatus(!menuStatus)
   }
+  
+  useEffect(()=>{
+    document.addEventListener("mousedown", (e) => {
+      //console.log(mainNavRef.current?.contains(e.target as HTMLElement ))
+      if(!mainNavRef.current?.contains(e.target as HTMLElement)){
+        setMenuStatus(false);
+      }
+    })
+  })
 
+  
   
 
   return (
     <nav>
-      <ul 
-        id="mainNav"
-        className={ isHidden +' fixed right-0 top-0 h-screen w-1/2 z-10 font-zen text-2xl bg-slate-700 bg-opacity-90 pt-30 md:relative md:flex md:flex-row md:w-auto md:h-auto md:bg-transparent md:space-x-0 lg:space-x-5  md:top-0 md:text-white lg:text-base md:text-sm'}
+      <ul ref={mainNavRef} id="mainNav"
+        className={ isHidden + ' fixed right-0 top-0 min-h-screen h-full w-1/2 z-10 font-zen text-2xl bg-slate-700 bg-opacity-90 pt-30 md:relative md:flex md:flex-row md:w-auto md:h-auto md:min-h-0 md:bg-transparent md:space-x-0 lg:space-x-5  md:top-0 md:text-white lg:text-base md:text-sm'}
       >
         <li className="nav-item">
           <NavLink to="/"><span>home</span></NavLink>
